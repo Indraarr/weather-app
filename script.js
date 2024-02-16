eb8016251cf29f10ab34606a6c2d750487dda223
Indraarr <indrabachtiar123@gmail.com>
2024-02-17 01:00:59 +0800

    waiting for api

---
+++ script.js
@@ -0,0 +1,4 @@
+const API = "85f556c6b63629766715d235610b2b72";
+
+// check if api has been activated here:
+// https://api.openweathermap.org/data/2.5/weather?q={germany}&appid={85f556c6b63629766715d235610b2b72}
